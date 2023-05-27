d747cffdad18129bfdb2908f29d56d97d8fa5e0b
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-05-27 16:25:10 +0530

    Added Trademark Footnote, Removed relaunch alert

--- snApp/Settings/AppearanceSettings.swift
+++ snApp/Settings/AppearanceSettings.swift
@@ -11,27 +11,32 @@ struct AppearanceSettings: View {
     @AppStorage("AppearanceStyle") var appearance: Appearance = .win11
     
     var body: some View {
-        Form {
-            Section {
-                Picker("Appearance Style", selection: $appearance) {
-                    ForEach(Appearance.allCases, id: \.self) { appearance in
-                        Text(appearance.rawValue)
-                            .tag(appearance)
+        VStack {
+            Form {
+                Section {
+                    Picker("Appearance Style", selection: $appearance) {
+                        ForEach(Appearance.allCases, id: \.self) { appearance in
+                            Text(appearance.rawValue)
+                                .tag(appearance)
+                        }
                     }
+                    .pickerStyle(.segmented)
                 }
-                .pickerStyle(.segmented)
-            } footer: {
-                Text("Relaunch the app for the changes to take effect.")
-                    .foregroundColor(.secondary)
             }
+            .formStyle(.grouped)
+            Text("Windows is a trademark of Microsoft Corporation\niPadOS is a trademark of Apple Inc.")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.horizontal, 5)
         }
-        .formStyle(.grouped)
     }
 }
 
 enum Appearance: String, Codable, CaseIterable {
-    case win11 = "Win 11"
-    case iPad = "iPad"
+    case win11 = "Windows"
+    case iPad = "iPadOS"
 }
 
 struct AppearanceSettings_Previews: PreviewProvider {
